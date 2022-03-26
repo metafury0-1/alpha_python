769a9b6448c28a11e10b6ea4b887c208fae474f9
malwarefury <iammurali19@gmail.com>
2022-03-26 09:28:14 +0530

    n parathensis problem

---
+++ algoproblems/n_parathensis.py
@@ -0,0 +1,51 @@
+# Generate Parentheses Problem
+# The problem is: Given N pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+# For example, given N = 3, a solution set is:
+
+# [
+#   "((()))",
+#   "(()())",
+#   "(())()",
+#   "()(())",
+#   "()()()"
+# ]
+# The naive solution is to generate all combinations of N pairs of parentheses, then checking if each one is valid.
+
+
+#solution
+# reference : https://www.youtube.com/watch?v=s9fokUqJ76A&t=109s&ab_channel=NeetCode
+# conditions
+# 1. openN=closedN=N
+# 2. open> close --> condition to add parathensis 
+# 3. valid if openN == closedN
+
+def generate_N_parathensis(N):
+    stack=[]
+    result=[]
+    def backtrack(openN, closedN):
+
+        # condition to return nothing 
+        if openN==closedN==N:
+            result.append("".join(stack))
+            return
+        
+        if openN < N:
+            stack.append("(")
+            backtrack(openN+1,closedN)
+            stack.pop()
+
+        if closedN <openN:
+            stack.append(")")
+            backtrack(openN,closedN+1)
+            stack.pop()
+        
+    backtrack(0,0)
+    return result
+
+
+print(generate_N_parathensis(3))
+
+        
+
+
